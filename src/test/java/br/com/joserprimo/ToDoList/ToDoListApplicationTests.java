531b5a1ef46cada3e4224e1c031e4f5d411a74b5
joserprimo <jose.primo.dev@gmail.com>
2026-02-02 18:20:31 -0300

    Fist commit

---
+++ src/test/java/br/com/joserprimo/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joserprimo.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
